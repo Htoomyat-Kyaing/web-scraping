fc78f58c9b2035dce142e8751e5e6aafe86632f1
Htoomyat-Kyaing <94776906+Htoomyat-Kyaing@users.noreply.github.com>
2024-01-01 18:47:44 +0630

    all page all books + bookDetails

--- index.js
+++ index.js
@@ -1,36 +1,86 @@
+import axios from "axios";
+import * as cheerio from 'cheerio';
+import fs from 'fs';
 import puppeteer from "puppeteer";
 
-const url = "https://www.pdfdrive.com/category/112"
+const url = "https://www.pdfdrive.com/category/112/";
 
-const main = async () => {
-	// Launch the browser and open a new blank page
-	const browser = await puppeteer.launch({ headless: "new" });
-	const page = await browser.newPage();
+const main = async (url) => {
+	let books = []
+	try {
+		const browser = await puppeteer.launch({ headless: "new" });
+		const page = await browser.newPage();
+		await page.goto(url, { waitUntil: "load" });
 
-	// Navigate the page to a URL
-	await page.goto(url);
+		const { data } = await axios.get(page.url());
+		const $ = cheerio.load(data);
+		const allPages = $(".Zebra_Pagination ul").find("li").length
 
-	const allBooks = await page.evaluate(() => {
-		const data = {}
+		for (let i = 0; i < allPages - 2; i++) {
+			console.log(`scraping ${page.url()}`)
+			await scrapeBooksForOnePage(page.url(), books)
+			await page.click(".Zebra_Pagination ul li a.next")
+		}
+		console.log(`Total ${books.length} books has been scraped`);
 
-		const category = document.querySelector(".collection-title").innerText
-		const booksDiv = document.querySelectorAll(".col-sm")
-		const books = []
-		booksDiv.forEach(book => {
-			const title = book.querySelector(".file-right > a > h2").innerText
-			const info = book.querySelector(".file-info").innerText
-			const imgURL = book.querySelector(".file-left > a > img").src
+		fs.writeFile("books.json", JSON.stringify(books, null, 2), (err) => {
+			if (err) {
+				console.error(err);
+				return;
+			}
+			console.log("Successfully written data to file");
+		});
 
-			books.push({ title, info, imgURL })
+		await browser.close();
+
+	} catch (error) {
+		console.error(error)
+	}
+}
+
+async function scrapeBooksForOnePage(url, books) {
+
+	const api = "https://www.pdfdrive.com"
+	try {
+		const { data } = await axios.get(url);
+		const $ = cheerio.load(data);
+		const booksList = $(".files-new").find(".col-sm")
+		booksList.each(async (_, el) => {
+			const book = { title: "", image: "", pageCount: "", publishedAt: "", fileSize: "", details: {} }
+			book.title = $(el).find("h2").text()
+			book.image = $(el).find("img").attr("src")
+			book.pageCount = $(el).find(".fi-pagecount").text()
+			book.publishedAt = $(el).find(".fi-year").text()
+			book.fileSize = $(el).find(".fi-size").text()
+			let endpoint = $(el).find(".file-right").children("a").attr("href")
+			book.details = await scrapDetailsForOneBook(endpoint)
+			books.push(book)
 		})
 
-		data[category] = books
-		return data
-	})
+	} catch (err) {
+		console.error(err);
+	}
+}
 
-	console.log(allBooks)
+async function scrapDetailsForOneBook(endpoint) {
+	const api = "https://www.pdfdrive.com"
+	try {
+		const { data } = await axios.get(api + endpoint)
+		const $ = cheerio.load(data);
+		const bookInfo = $(".ebook-right")
+		const bookDetails = { language: "", tags: [], downloadLink: "" }
+		bookDetails.language = bookInfo.find(".ebook-file-info span:last").text()
+		bookInfo.find(".ebook-tags a").each((_, a) => {
+			bookDetails.tags.push($(a).text())
+		})
+		const bookButtons = $(".ebook-buttons")
+		bookDetails.downloadLink = api + bookButtons.find("span a").attr("href")
+		return { ...bookDetails };
+	}
+	catch (err) {
+		console.error(err)
+	}
+}
 
-	await browser.close();
-};
-
-main()
+// Main function call
+main(url)
